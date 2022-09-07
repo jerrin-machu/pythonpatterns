a30f475e50c7f2d019a05d53efcc5163d54f36d5
Jerrin Thomas <jerrinmachu@gmail.com>
2022-09-07 15:38:07 +0530

    squre pattern program completed

---
+++ squrepattern.py
@@ -0,0 +1,14 @@
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+n =5
+string = ""
+for i in range(0,n):
+    for j in range(0,n):
+        string = string + "*"
+    string = string + "\n"
+
+
+print(string)
