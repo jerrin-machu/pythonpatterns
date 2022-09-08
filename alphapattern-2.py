0e28bb70fea913add5d9a4cab4f6de6e917a532b
Jerrin Thomas <jerrinmachu@gmail.com>
2022-09-08 14:00:07 +0530

    pattern programs added to the git

--- alphapattern-2.py
+++ alphapattern-2.py
@@ -1,8 +1,8 @@
 n = 5
 string =""
 for i in range(1,n+1):
-    for j in range(0,n-i+1):
-        string = string + chr(j + 65)
+    for j in range(0,n-i +1):
+        string = string + chr((n-1-j) +65)
     string = string + "\n"
 
 print(string)
