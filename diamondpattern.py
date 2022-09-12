f146948b8cd8e9e5a96bc8f5975ee052b9ff6827
Jerrin Thomas <jerrinmachu@gmail.com>
2022-09-13 03:18:20 +0530

    diamond pattern added

---
+++ diamondpattern.py
@@ -0,0 +1,17 @@
+n = 5
+string = ""
+for i in range(1, n+1):
+    for j in range(n,i,-1):
+        string = string + " "
+    for k in range(0, 2*i -1):
+        string = string + "*"
+    string = string + "\n"
+
+for i in range(1,n-1+1):
+    for j in range(0,i):
+        string = string + " "
+    for k in range((n-i)*2 - 1,0, -1):
+        string = string + "*"
+    string = string + "\n"
+
+print(string)
